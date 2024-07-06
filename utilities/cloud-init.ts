4ea58ab2ea3b3f2814f626ed8f8310253ad3c5e7
Aiden Vigue <aiden@vigue.me>
2024-07-06 18:58:16 -0400

    add WS library

--- utilities/cloud-init.ts
+++ utilities/cloud-init.ts
@@ -12,7 +12,7 @@ RUNNER_VERSION=2.317.0
 apt update -y
 apt install -y gawk wget git diffstat unzip texinfo gcc build-essential chrpath socat cpio \
 python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 \
-python3-subunit zstd liblz4-tool file locales libacl1
+python3-subunit zstd liblz4-tool file locales libacl1 python3-websocket
 
 locale-gen en_US.UTF-8
 
